615d6f136704fa830ca387d9886a5c012d8ea77e
Sean <s1210593@gmail.com>
2018-01-05 11:56:02 +0000

    function added to set date of next ozlan by providing 4 parameters. function added to return details of next ozlan taking place.

--- reebot/reebot.js
+++ reebot/reebot.js
@@ -1,16 +1,46 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+
+const fs = require('fs');
+
+const config = require('../config.json');
+const prefix = config.prefix;
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', (message) => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
     
-    if (message.content.startsWith(config.prefix + 'ping')) {
-        message.channel.send('pong!');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    if (message.content.startsWith(prefix)) {
+        switch(command) {
+            case 'newlan' :
+                if (message.author.id !== config.ownerId) return;
+
+                let [name, start, end, month] = args;
+                config.lanName = name;
+                config.lanStart = start;
+                config.lanEnd = end;
+                config.lanMonth = month;
+
+                fs.writeFile('../config.json', JSON.stringify(config, null, 4), (err) => console.error);
+
+                message.channel.send('Got it!');
+                break;
+        
+            case 'ozlanwhen' :
+                const lanName = config.lanName;
+                const lanStart = config.lanStart;
+                const lanEnd = config.lanEnd;
+                const lanMonth = config.lanMonth;
+
+                message.reply(`The next OzLAN is ${lanName}, taking place from ${lanStart} to ${lanEnd} of ${lanMonth}.`);
+                break;
+       }
     }
 });
 
