150fbf4a7ee74cd18bd635ddafe8066e29892f15
Sean <s1210593@gmail.com>
2018-01-05 09:58:49 +0000

    initial commit

--- reebot/reebot.js
+++ reebot/reebot.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', (message) => {
-    if (message.content.startsWith('ping')) {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    
+    if (message.content.startsWith(config.prefix + 'ping')) {
         message.channel.send('pong!');
     }
 });
 
-client.login('MzkzMzQzMDMxMzcxMTA0MjU4.DR0mBQ.E4XRq3VaRPM70SXU2MhV_WdUixo');
+client.login(config.token);
