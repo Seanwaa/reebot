6bee9606136a5c141faae57d79a66376347237b5
Sean <s1210593@gmail.com>
2018-01-26 13:36:52 +0000

    debugging

--- reebot/reebot.js
+++ reebot/reebot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const config = require("./config.json");
+const config = require('../config.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
